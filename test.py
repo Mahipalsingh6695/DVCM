eefa941cf36139d35779708b84dac61461b38283
Mahipal Singh <mahipalsinghchauhan1995@gmail.com>
2023-12-12 18:40:33 +0530

    fourth commit

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ data=[
     {"name":"sunny","age":27,"city":"bhopal"},
     {"name":"Mahipal","age":27,"city":"bhim"},
     {"name":"Ravi","age":29,"city":"beawar"},
+    {"name":"shagun","age":25,"city":"bhinay"}
 ]
 
 df=pd.DataFrame(data)
